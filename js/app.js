ae26223c24b035e8e93c76ada9699bbfabe84514
Swastik <65583094+29swastik@users.noreply.github.com>
2022-11-29 13:17:04 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -129,8 +129,7 @@ var createScene = function () {
 
     light.intensity = 1.5;
 
-    await Ammo();
-    scene.enablePhysics(new BABYLON.Vector3(0,-9.81, 0), new BABYLON.AmmoJSPlugin());	
+    Ammo().then(() => {scene.enablePhysics(new BABYLON.Vector3(0,-9.81, 0), new BABYLON.AmmoJSPlugin())};
 
     var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 10, height: 50}, scene);
     ground.scaling = new BABYLON.Vector3(1, 1, -50);
